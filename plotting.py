57530cedb4ef7030d685b2b24ac592509c97d62f
Pascal Engeli <pascal.engeli@uzh.ch>
2021-02-09 21:51:56 +0100

    Added different thresholds depending on FPS. Added some lines in tracker and counting systems for debugging

--- plotting.py
+++ plotting.py
@@ -82,6 +82,8 @@ plt.show()
 
 
 
+
+
 # Basic Configuration
 fig, axes = plt.subplots(ncols=2, figsize=(12, 12))
 ax1, ax2 = axes
@@ -114,5 +116,76 @@ fig.tight_layout()
 plt.show()
 
 
+import matplotlib.pyplot as plt
 
+matches = []
+for i in total_matches:
+    for j in i:
+        matches.append(j)
+
+no_matches = []
+for i in total_no_matches:
+    for j in i:
+        no_matches.append(j)
+
+all = matches + no_matches
+
+colors = []
+for i in all:
+    if i in matches:
+        colors.append('g')
+    if i in no_matches:
+        colors.append('r')
+
+x, y = zip(*all)
+
+for xa, ya, c in zip(x, y, colors):
+    plt.scatter(xa, ya, color=c)
+
+plt.xticks(np.arange(min(x),max(x), max(x)//10))
+plt.yticks(np.arange(0.0,1.04, 0.05))
+
+plt.show()
+
+
+def get_iou_score(box1: np.ndarray, box2: np.ndarray):
+    """
+    calculate intersection over union cover percent
+    :param box1: box1 with shape (N,4) or (N,2,2) or (2,2) or (4,). first shape is preferred
+    :param box2: box2 with shape (N,4) or (N,2,2) or (2,2) or (4,). first shape is preferred
+    :return: IoU ratio if intersect, else 0
+    """
+    # first unify all boxes to shape (N,4)
+    if box1.shape[-1] == 2 or len(box1.shape) == 1:
+        box1 = box1.reshape(1, 4) if len(box1.shape) <= 2 else box1.reshape(box1.shape[0], 4)
+    if box2.shape[-1] == 2 or len(box2.shape) == 1:
+        box2 = box2.reshape(1, 4) if len(box2.shape) <= 2 else box2.reshape(box2.shape[0], 4)
+    point_num = max(box1.shape[0], box2.shape[0])
+    b1p1, b1p2, b2p1, b2p2 = box1[:, :2], box1[:, 2:], box2[:, :2], box2[:, 2:]
+
+    # mask that eliminates non-intersecting matrices
+    base_mat = np.ones(shape=(point_num,))
+    base_mat *= np.all(np.greater(b1p2 - b2p1, 0), axis=1)
+    base_mat *= np.all(np.greater(b2p2 - b1p1, 0), axis=1)
+
+    # I area
+    intersect_area = np.prod(np.minimum(b2p2, b1p2) - np.maximum(b1p1, b2p1), axis=1)
+    # U area
+    union_area = np.prod(b1p2 - b1p1, axis=1) + np.prod(b2p2 - b2p1, axis=1) - intersect_area
+    # IoU
+    if union_area.all():
+        intersect_ratio = intersect_area / union_area
+    else:
+        intersect_ratio = 0
+
+    return base_mat * intersect_ratio
+
+
+from scipy.spatial import distance as dist
+dist.cdist(np.array(objectCoordinates), np.array(objectCoordinates),'euclidean')
+
+zweihundert = [[[800, 473, 840, 558]],[[803, 458, 843, 529]]]
+einhundert = [[[800, 473, 840, 558]],[[807, 441, 847, 512]]]
+füfzg = [[[800, 473, 840, 558]],[[808, 410, 853, 475]]]
+füfezwenzg = [[[800, 473, 840, 558]],[[814, 345, 854, 400]]]
 
